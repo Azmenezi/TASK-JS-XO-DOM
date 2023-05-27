f386bba9adb7f9f1811b5ef363a2bbeb06786327
Azmenezi <azmenezi@gmail.com>
2023-05-27 20:03:44 +0100

    Fixed impossible

--- Scripts/harder.js
+++ Scripts/harder.js
@@ -180,24 +180,19 @@ function bestMove() {
       }
     }
   }
-  // Try to take the center
-  if (!clicked_X.includes(5) && !clicked_O.includes(5)) {
-    oPlayer(5);
-    return;
-  }
-
   let bCorners = [2, 4, 6, 8];
   let corners = [1, 3, 7, 9];
   // Try to take a corner
   for (let i = 0; i < bCorners.length; i++) {
-    if (
-      clicked_X.includes(corners[i]) &&
-      !clicked_O.includes(bCorners[i]) &&
-      !clicked_X.includes(bCorners[i])
-    ) {
+    if (clicked_X.includes(corners[i]) && !clicked_O.includes(bCorners[i])) {
       oPlayer(bCorners[i]);
       return;
     }
+  }
+  // Try to take the center
+  if (!clicked_X.includes(5) && !clicked_O.includes(5)) {
+    oPlayer(5);
+    return;
   }
 
   shuffle(corners);
